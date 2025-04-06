a811915a1a2a547b6908050eca1dc9849a4341de
Abdoulllahi <dabdoulahi@gmail.com>
2025-04-05 23:12:19 -0500

    basics

---
+++ Server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000,
+    () => {
+        console.log("Server listening on port 3000...");
+    }
+);
+
+app.get("/", (request, response) => {
+    response.send("Hello from Server")
+})
