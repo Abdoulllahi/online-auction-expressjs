03a91c324ed298c2f80fd2f0bed922a6e21e54d8
Abdoulllahi <dabdoulahi@gmail.com>
2025-04-09 19:38:31 -0500

    virtual bid

--- Server/server.js
+++ Server/server.js
@@ -11,6 +11,7 @@ app.use((request, response, next) => {
 
 app.listen(3000,
     () => {
+        setInterval(virtualBid, 5000);
         console.log("Server listening on port 3000...");
     }
 );
@@ -19,3 +20,13 @@ app.get("/list", (request, response) => {
     console.log("GET: /list received @ " + (new Date(Date.now())).toISOString());
     response.send(dbJson);
 })
+
+function virtualBid() {
+    
+    dbJson.forEach((item) => {
+        
+        const increase = Math.random() * 2;
+        item.lastBid *= Number(1.0 + (increase / 100));
+        item.lastBidUser = "anonymous";
+    })
+}
